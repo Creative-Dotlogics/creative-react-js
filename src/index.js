2881b74de59032c2880ed1be8b979f5f5d957165
Hamzaweedseed8 <humar6078@gmail.com>
2019-06-08 16:55:42 +0000

    modal upgrade

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import MaterialInput from './components/MaterialInput';
 import MaterialNavbar from './components/MaterialNavbar';
 import MaterialModal from './components/MaterialModal';
+import MaterialButton from './components/MaterialButton';
 
-export { MaterialInput, MaterialNavbar, MaterialModal }
+export { MaterialInput, MaterialNavbar, MaterialModal, MaterialButton }
